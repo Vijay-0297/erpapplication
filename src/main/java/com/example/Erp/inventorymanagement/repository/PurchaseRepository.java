5ec079d1a726ad2f73a679b6a50c32eda66ead5e
Vijay-0297 <vijaydev0297@gmail.com>
2026-08-31 16:01:06 +0530

    commit new code

--- src/main/java/com/example/Erp/inventorymanagement/repository/PurchaseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Erp.inventorymanagement.repository;
-
-import com.example.Erp.inventorymanagement.model.Purchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseRepository extends JpaRepository<Purchase,Integer> {
-
-}
--- src/main/java/com/example/Erp/inventorymanagement/repository/SuppliersRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Erp.inventorymanagement.repository;
-
-import com.example.Erp.inventorymanagement.model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SuppliersRepository extends JpaRepository<Supplier, Integer> {
-
-}
